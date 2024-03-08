95f004355c2fdfb0ecd55643cc8b7a401009e26e
Nadi <nadisoepaing9@gmail.com>
2024-03-08 13:33:57 +0630

    added changes to MainEntry.java

--- src/main/MainEntry.java
+++ src/main/MainEntry.java
@@ -7,6 +7,7 @@ public class MainEntry {
 		System.out.println("Application is started");
 		System.out.println("Make Some Changes...");
 		System.out.println("Make changed 2");
+		System.out.println("Latest Changed...");
 	}
 
 }
